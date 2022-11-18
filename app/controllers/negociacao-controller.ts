8bfb2ea58a1b00bdb864168537f24e51b21b4167
Anthony Guilherme <anthonytguilherme@hotmail.com>
2022-11-18 16:16:14 -0300

    Better organize our code

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -12,12 +12,16 @@ export class NegociacaoController {
     this.inputValor = document.querySelector('#valor');
   }
 
-  adiciona() {
-    const negociacao = new Negociacao(
-      this.inputData.value,
-      this.inputQuantidade.value,
-      this.inputValor.value
-    );
+  adiciona(): void {
+    const negociacao = this.criaNegociacao();
     console.log(negociacao)
+  }
+
+  criaNegociacao(): Negociacao {
+    const exp = /-/g;
+    const date = new Date(this.inputData.value.replace(exp, ','));
+    const quantidade = parseInt(this.inputQuantidade.value);
+    const valor = parseFloat(this.inputValor.value);
+    return new Negociacao(date, quantidade, valor);
   }
 }
